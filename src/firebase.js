0808523786913e222c88a2e5424d5f772979f8c3
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-11 13:43:36 +0300

    Header and Sidebar Components are updated

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQASCHoRvh4OefqV_YKxEI27piJ0j3OD8",
+  authDomain: "slack-clone-285aa.firebaseapp.com",
+  projectId: "slack-clone-285aa",
+  storageBucket: "slack-clone-285aa.appspot.com",
+  messagingSenderId: "591726451545",
+  appId: "1:591726451545:web:450e0974df18a91b604a48",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
